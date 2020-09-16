def23efa6454c40b6f8daa599e92994fed6e0924
abhilash <abhidenim10@gmail.com>
2020-09-16 15:51:38 +0530

    first commit

---
+++ dp29app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.from django.contrib import admin
+from dp29app import models
+
+class TopicAdminView(admin.ModelAdmin):
+    list_display=('topic_name',)
+    search_fields=('topic_name',)
+    
+class WebPageAdminView(admin.ModelAdmin):
+    list_display=('topic','name','url')
+    search_fields=('name',)
+    list_editable=('name',)
+    list_filter=('topic',)
+
+class AccessDetailsAdminView(admin.ModelAdmin):
+    list_display=('webpage','datetime')
+
+admin.site.register(models.Topic,TopicAdminView)
+admin.site.register(models.WebPage,WebPageAdminView)
+admin.site.register(models.AccessDetails,AccessDetailsAdminView)
---
+++ dp29app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Dp29AppConfig(AppConfig):
+    name = 'dp29app'
